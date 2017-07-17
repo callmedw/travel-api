f48d16c20a863c4d174a0d5016b18d78406a8457
Susha Dore and Sara Schultz <sara@>
2017-07-17 11:29:27 -0700

    add json response for specific destination call

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -5,6 +5,26 @@ class DestinationsController < ApplicationController
     json_response(@destinations)
   end
 
+  def show
+    @destination = Destination.find(params[:id])
+    json_response(@destination)
+  end
+
+  # def create
+  #   @quote = Quote.create(quote_params)
+  #   json_response(@quote)
+  # end
+  #
+  # def update
+  #   @quote = Quote.find(params[:id])
+  #   @quote.update(quote_params)
+  # end
+  #
+  # def destroy
+  #   @quote = Quote.find(params[:id])
+  #   @quote.destroy
+  # end
+
   private
   def json_response(object)
     render json: object.as_json(include: :reviews), status: :ok
