e8ec18085929a0b5b8f6426e854ed799776add61
Susha Dore and Sara Schultz <sara@>
2017-07-18 16:02:17 -0700

    pass spec for delete_reviews request

---
+++ spec/requests/delete_reviews_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "delete review route", :type => :request do
+
+  before do
+    destination = Destination.create(location: 'test', id: 1)
+    review = Review.create(author: 'Sway', body: 'test review', rating: 2, destination_id: 1, id: 1)
+    delete "/destinations/#{destination.id}/reviews/#{review.id}"
+  end
+
+  it 'destroys destination' do
+    expect(response).to have_http_status 204
+  end
+end
