223a51359aa533b644ee150b6d98c612d2bfde44
Susha Dore and Sara Schultz <sara@>
2017-07-18 15:56:57 -0700

    pass spec for post_reviews request

---
+++ spec/requests/post_reviews_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "post review route", :type => :request do
+
+  before do
+    destination = Destination.create(location: 'test', id: 1)
+    post "/destinations/#{destination.id}/reviews", params: { author: 'Sway', body: 'test review', rating: 2, destination_id: 1 }
+  end
+
+  it 'returns the review' do
+    expect(JSON.parse(response.body)['location']).to eq "test"
+  end
+
+  it 'returns created status' do
+    expect(response).to have_http_status 200
+  end
+end
