4dbbc52bc3e49ad7a63f78b62cffb4bffe9a3c86
Dana Weiss and Sara Schultz <sara@>
2017-07-20 09:31:31 -0700

    pass spec for not_found exception

--- spec/requests/get_destinations_exceptions_spec.rb
+++ spec/requests/get_destinations_exceptions_spec.rb
@@ -1,12 +1,12 @@
-# require 'rails_helper'
-#
-# describe 'get a destination route', :type => :request do
-#
-#   before do
-#     get '/destinations/100'
-#   end
-#
-#   it 'returns an error if no destination is found' do
-#     expect(response).to have_http_status(:not_found)
-#   end
-# end
+require 'rails_helper'
+
+describe 'get a destination route', :type => :request do
+
+  before do
+    get '/destinations/31'
+  end
+
+  it 'returns an error if no destination is found' do
+    expect(response).to have_http_status(:not_found)
+  end
+end
